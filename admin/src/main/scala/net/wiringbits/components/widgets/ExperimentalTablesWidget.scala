d5ded61a9cc296439a2979c3d3ad043fa1406e37
caiseL <cristophertraperoo@gmail.com>
2021-09-21 21:06:16 -0600

    Changes to names

--- admin/src/main/scala/net/wiringbits/components/widgets/ExperimentalTablesWidget.scala
+++ admin/src/main/scala/net/wiringbits/components/widgets/ExperimentalTablesWidget.scala
@@ -1,16 +1,12 @@
 package net.wiringbits.components.widgets
 
-import com.alexitc.materialui.facade.materialUiCore.{components => mui, materialUiCoreStrings => muiStrings}
 import net.wiringbits.api.models.AdminGetTablesResponse
 import net.wiringbits.ui.components.core.RemoteDataLoader
-import net.wiringbits.{API, AppStrings}
-import net.wiringbits.ui.components.core.widgets.{CircularLoader, Container}
-import net.wiringbits.ui.core.GenericHooks
+import net.wiringbits.API
 import slinky.core.FunctionalComponent
 import slinky.core.annotations.react
-import slinky.core.facade.Fragment
 
-@react object Tables {
+@react object ExperimentalTablesWidget {
   case class Props(api: API)
 
   val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props =>
@@ -19,19 +15,9 @@ import slinky.core.facade.Fragment
         .Props(
           fetch = () => props.api.client.adminGetTables(),
           render = response => ExperimentalTableListWidget.component(ExperimentalTableListWidget.Props(response)),
-          progressIndicator = () => loader
+          progressIndicator = () => Loader()
         )
     )
   }
-
-  private def loader = Container(
-    flex = Some(1),
-    alignItems = Container.Alignment.center,
-    justifyContent = Container.Alignment.center,
-    child = Fragment(
-      CircularLoader(),
-      mui.Typography(AppStrings.loading).variant(muiStrings.h6)
-    )
-  )
 
 }
