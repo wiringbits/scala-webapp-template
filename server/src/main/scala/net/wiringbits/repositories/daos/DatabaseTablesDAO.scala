a4342042d48f25dc4398f5bc524fcd2b5aedf792
caiseL <cristophertraperoo@gmail.com>
2021-09-28 19:34:43 -0600

    Method to get data and metadata from DB separated in two

--- server/src/main/scala/net/wiringbits/repositories/daos/DatabaseTablesDAO.scala
+++ server/src/main/scala/net/wiringbits/repositories/daos/DatabaseTablesDAO.scala
@@ -8,54 +8,60 @@ import scala.collection.mutable.ListBuffer
 
 object DatabaseTablesDAO {
 
-  def all()(implicit conn: Connection): List[DatabaseTable] = {
+  def all(schema: String = "public")(implicit conn: Connection): List[DatabaseTable] = {
     // TODO: Not generic enough
     SQL"""
       SELECT table_name
       FROM information_schema.tables
-      WHERE table_schema='public'
-      AND table_type='BASE TABLE'
+      WHERE table_schema=$schema
+        AND table_type='BASE TABLE'
       ORDER BY table_name
       """.as(tableParser.*)
   }
 
   def getTableMetadata(tableName: String)(implicit conn: Connection): TableMetadata = {
-    val tableData = new ListBuffer[RowMetadata]()
-    val columnsMetadata = new ListBuffer[ColumnMetadata]()
-
     val statement = conn.createStatement(ResultSet.CONCUR_READ_ONLY, ResultSet.CONCUR_READ_ONLY)
     val resultSet = statement.executeQuery("SELECT * FROM " + tableName)
-    val metadata = resultSet.getMetaData
+    try {
+      val metadata = resultSet.getMetaData
+      val numberOfColumns = metadata.getColumnCount
 
-    val numberOfColumns = metadata.getColumnCount
+      val columnsMetadata = for {
+        columnNumber <- 1 to numberOfColumns
+        columnName = metadata.getColumnName(columnNumber)
+        columnType = metadata.getColumnTypeName(columnNumber)
+        columnMetadata = ColumnMetadata(columnName, columnType)
+      } yield columnMetadata
 
-    for (columnNumber <- 1 to numberOfColumns) {
-      val columnName = metadata.getColumnName(columnNumber)
-      val columnType = metadata.getColumnTypeName(columnNumber)
-      val columnMetadata = ColumnMetadata(columnName, columnType)
-      columnsMetadata += columnMetadata
+      val tableData = getTableData(resultSet)
+      TableMetadata(tableName, columnsMetadata.toList, tableData.toList)
+    } finally {
+      resultSet.close()
+      statement.close()
     }
+
+  }
+
+  def getTableData(resultSet: ResultSet): ListBuffer[RowMetadata] = {
+    val tableData = new ListBuffer[RowMetadata]()
+    val metadata = resultSet.getMetaData
+    val numberOfColumns = metadata.getColumnCount
 
     // It goes into the rows one by one
     while (resultSet.next) {
-      val rowData = new ListBuffer[Cell]
-      for (columnNumber <- 1 to numberOfColumns) {
-        val columnName = metadata.getColumnName(columnNumber)
-
-        val data = resultSet.getString(columnName)
+      val rowData = for {
+        columnNumber <- 1 to numberOfColumns
+        columnName = metadata.getColumnName(columnNumber)
+        data = resultSet.getString(columnName)
 
         // This is just a workaround. I think it'll be better if I use a Option[T] syntax
         // so I'll do it later
-        val cell = if (data == null) Cell("null") else Cell(data)
-        rowData += cell
-      }
+        cell = if (data == null) Cell("null") else Cell(data)
+      } yield cell
+
       tableData += RowMetadata(rowData.toList)
     }
-
-    statement.close()
-    resultSet.close()
-    val tableMetadata = TableMetadata(tableName, columnsMetadata.toList, tableData.toList)
-
-    tableMetadata
+    tableData
   }
+
 }
