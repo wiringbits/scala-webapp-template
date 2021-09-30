656946d7bb8f48c48b16f6ccc6703abd778a66a7
caiseL <cristophertraperoo@gmail.com>
2021-09-30 15:44:05 -0600

    Added new tests to server DB tables endpoint

--- server/src/test/scala/controllers/AdminControllerSpec.scala
+++ server/src/test/scala/controllers/AdminControllerSpec.scala
@@ -2,6 +2,8 @@ package controllers
 
 import controllers.common.PlayPostgresSpec
 
+import scala.util.control.NonFatal
+
 class AdminControllerSpec extends PlayPostgresSpec {
 
   "GET /admin/tables/users" should {
@@ -11,19 +13,42 @@ class AdminControllerSpec extends PlayPostgresSpec {
       response.columns mustNot be(empty)
     }
 
-    /*
-    "fail when database doesn't exists" in withApiClient { client =>
+    "fail when you enter negative offset" in withApiClient { client =>
       val error = client
-        .adminGetTableMetadata("adklsadasldklda")
+        .adminGetTableMetadata("users", -1, 10)
         .map(_ => "Success when failure expected")
         .recover { case NonFatal(ex) =>
           ex.getMessage
         }
         .futureValue
-      error must be("Invalid table")
 
+      error must be(s"You can't query a table using negative numbers as a limit or offset")
     }
-     */
+
+    "fail when you enter negative limit" in withApiClient { client =>
+      val error = client
+        .adminGetTableMetadata("users", 0, -1)
+        .map(_ => "Success when failure expected")
+        .recover { case NonFatal(ex) =>
+          ex.getMessage
+        }
+        .futureValue
+
+      error must be(s"You can't query a table using negative numbers as a limit or offset")
+    }
   }
 
+  "GET /admin/tables/aaaaaaaaaaa" should {
+    "fail when database doesn't exists" in withApiClient { client =>
+      val invalidTableName = "aaaaaaaaaaa"
+      val error = client
+        .adminGetTableMetadata(invalidTableName, 0, 10)
+        .map(_ => "Success when failure expected")
+        .recover { case NonFatal(ex) =>
+          ex.getMessage
+        }
+        .futureValue
+      error must be(s"Unexpected error because the DB table wasn't found: $invalidTableName")
+    }
+  }
 }
