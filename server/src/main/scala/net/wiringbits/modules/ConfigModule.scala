2077742563e67b788f96c4c5be37ea294724514a
Cristopher Orona <cristophertraperoo@gmail.com>
2022-01-18 17:02:43 -0700

    Log masked secrets when the server starts (#107)

--- server/src/main/scala/net/wiringbits/modules/ConfigModule.scala
+++ server/src/main/scala/net/wiringbits/modules/ConfigModule.scala
@@ -2,12 +2,17 @@ package net.wiringbits.modules
 
 import com.google.inject.{AbstractModule, Provides}
 import net.wiringbits.config.JwtConfig
+import org.slf4j.LoggerFactory
 import play.api.Configuration
 
 class ConfigModule extends AbstractModule {
 
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
   @Provides()
   def jwtConfig(global: Configuration): JwtConfig = {
-    JwtConfig(global.get[Configuration]("jwt"))
+    val config = JwtConfig(global.get[Configuration]("jwt"))
+    logger.info(s"Loading jwtConfig, secret = ${config.secret}")
+    config
   }
 }
