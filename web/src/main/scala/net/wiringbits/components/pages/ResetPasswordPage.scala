e28c3b73363fad28ef0ed64bb0289f9a4d5c6c9a
caiseL <cristophertraperoo@gmail.com>
2022-01-24 21:50:42 -0700

    Add reset password page and form

---
+++ web/src/main/scala/net/wiringbits/components/pages/ResetPasswordPage.scala
@@ -0,0 +1,87 @@
+package net.wiringbits.components.pages
+
+import com.alexitc.materialui.facade.csstype.mod.FlexDirectionProperty
+import com.alexitc.materialui.facade.materialUiCore.createMuiThemeMod.Theme
+import com.alexitc.materialui.facade.materialUiCore.{components => mui, materialUiCoreStrings => muiStrings}
+import com.alexitc.materialui.facade.materialUiIcons.{components => muiIcons}
+import com.alexitc.materialui.facade.materialUiStyles.makeStylesMod.StylesHook
+import com.alexitc.materialui.facade.materialUiStyles.mod.makeStyles
+import com.alexitc.materialui.facade.materialUiStyles.withStylesMod.{
+  CSSProperties,
+  StyleRulesCallback,
+  Styles,
+  WithStylesOptions
+}
+import net.wiringbits.components.widgets.ResetPasswordForm
+import net.wiringbits.webapp.utils.slinkyUtils.components.core.widgets.Container
+import net.wiringbits.{API, AppStrings}
+import org.scalablytyped.runtime.StringDictionary
+import slinky.core.FunctionalComponent
+import slinky.core.annotations.react
+import slinky.core.facade.Fragment
+import slinky.web.html.{className, div}
+import typings.reactRouter.mod.{useHistory, useParams}
+
+import scala.scalajs.js
+
+@react object ResetPasswordPage {
+  case class Props(api: API)
+
+  private lazy val useStyles: StylesHook[Styles[Theme, Unit, String]] = {
+    val stylesCallback: StyleRulesCallback[Theme, Unit, String] = theme =>
+      StringDictionary(
+        "resetPasswordPage" -> CSSProperties()
+          .setFlex(1)
+          .setDisplay("flex")
+          .setFlexDirection(FlexDirectionProperty.column)
+          .setAlignItems("center")
+          .setJustifyContent("center")
+          .setBackgroundColor("#ebeff2"),
+        "resetPasswordFormContainer" -> CSSProperties()
+          .setMaxWidth(400)
+          .setWidth("100%")
+      )
+    makeStyles(stylesCallback, WithStylesOptions())
+  }
+
+  val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props =>
+    val classes = useStyles(())
+    val history = useHistory()
+    val params = useParams()
+    val resetPasswordCode = Option(params.asInstanceOf[js.Dynamic].resetPasswordCode.toString)
+
+    div(className := classes("resetPasswordPage"))(
+      div(className := classes("resetPasswordFormContainer"))(
+        mui.Paper()(
+          Fragment(
+            Container(
+              flexDirection = Container.FlexDirection.row,
+              alignItems = Container.Alignment.center,
+              child = Fragment(
+                mui
+                  .IconButton(muiIcons.ChevronLeft().fontSize(muiStrings.large))
+                  .onClick(_ => history.push("/")),
+                mui.Typography(AppStrings.enterNewPassword).variant(muiStrings.h5)
+              )
+            ),
+            ResetPasswordForm(props.api, resetPasswordCode),
+            Container(
+              margin = Container.EdgeInsets.top(8),
+              flexDirection = Container.FlexDirection.row,
+              alignItems = Container.Alignment.center,
+              justifyContent = Container.Alignment.center,
+              child = Fragment(
+                mui.Typography(AppStrings.alreadyHaveAccount),
+                mui
+                  .Button(AppStrings.signIn)
+                  .variant(muiStrings.text)
+                  .color(muiStrings.primary)
+                  .onClick(_ => history.push("/signin"))
+              )
+            )
+          )
+        )
+      )
+    )
+  }
+}
---
+++ web/src/main/scala/net/wiringbits/components/widgets/ResetPasswordForm.scala
@@ -0,0 +1,128 @@
+package net.wiringbits.components.widgets
+
+import com.alexitc.materialui.facade.materialUiCore.{components => mui, materialUiCoreStrings => muiStrings}
+import net.wiringbits.forms.ResetPasswordFormData
+import net.wiringbits.ui.components.inputs.PasswordInput
+import net.wiringbits.webapp.utils.slinkyUtils.components.core.ErrorLabel
+import net.wiringbits.webapp.utils.slinkyUtils.components.core.widgets.{CircularLoader, Container}
+import net.wiringbits.webapp.utils.slinkyUtils.forms.StatefulFormData
+import net.wiringbits.{API, AppStrings}
+import org.scalajs.dom
+import org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits.global
+import slinky.core.annotations.react
+import slinky.core.facade.{Fragment, Hooks}
+import slinky.core.{FunctionalComponent, SyntheticEvent}
+import slinky.web.html.{form, onSubmit}
+import typings.reactRouter.mod.useHistory
+
+import scala.util.{Failure, Success}
+
+@react object ResetPasswordForm {
+  case class Props(api: API, token: Option[String])
+
+  val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props =>
+    val history = useHistory()
+
+    val (formData, setFormData) = Hooks.useState(
+      StatefulFormData(
+        ResetPasswordFormData.initial(
+          passwordLabel = AppStrings.password,
+          repeatPasswordLabel = AppStrings.repeatPassword,
+          token = props.token
+        )
+      )
+    )
+
+    def onDataChanged(f: ResetPasswordFormData => ResetPasswordFormData): Unit = {
+      setFormData { current =>
+        current.filling.copy(data = f(current.data))
+      }
+    }
+
+    def handleSubmit(e: SyntheticEvent[_, dom.Event]): Unit = {
+      e.preventDefault()
+
+      if (formData.isSubmitButtonEnabled) {
+        setFormData(_.submit)
+        for {
+          request <- formData.data.submitRequest
+            .orElse {
+              setFormData(_.submissionFailed("Complete the necessary data"))
+              None
+            }
+        } yield props.api.client
+          .resetPassword(request)
+          .onComplete {
+            case Success(_) =>
+              setFormData(_.submitted)
+              history.push("/signin")
+            case Failure(ex) =>
+              setFormData(_.submissionFailed(ex.getMessage))
+          }
+      } else {
+        println("Submit fired when it is not available")
+      }
+    }
+
+    val passwordInput = PasswordInput
+      .component(
+        PasswordInput.Props(
+          formData.data.password,
+          disabled = formData.isInputDisabled,
+          onChange = value => onDataChanged(x => x.copy(password = x.password.updated(value)))
+        )
+      )
+
+    val repeatPasswordInput = PasswordInput
+      .component(
+        PasswordInput.Props(
+          formData.data.repeatPassword,
+          disabled = formData.isInputDisabled,
+          onChange = value => onDataChanged(x => x.copy(repeatPassword = x.repeatPassword.updated(value)))
+        )
+      )
+
+    val resetPasswordButton = {
+      val text = if (formData.isSubmitting) {
+        Fragment(
+          CircularLoader(),
+          Container(margin = Container.EdgeInsets.left(8), child = AppStrings.loading)
+        )
+      } else {
+        Fragment(AppStrings.resetPassword)
+      }
+
+      mui
+        .Button(text)
+        .fullWidth(true)
+        .disabled(formData.isSubmitButtonDisabled)
+        .variant(muiStrings.contained)
+        .color(muiStrings.primary)
+        .size(muiStrings.large)
+        .`type`(muiStrings.submit)
+    }
+
+    form(onSubmit := (handleSubmit(_)))(
+      Container(
+        margin = Container.EdgeInsets.all(16),
+        alignItems = Container.Alignment.center,
+        child = Fragment(
+          passwordInput,
+          repeatPasswordInput,
+          formData.firstValidationError.map { text =>
+            Container(
+              margin = Container.EdgeInsets.top(16),
+              child = ErrorLabel(text)
+            )
+          }
+        )
+      ),
+      Container(
+        alignItems = Container.Alignment.center,
+        child = Fragment(
+          resetPasswordButton
+        )
+      )
+    )
+  }
+}
