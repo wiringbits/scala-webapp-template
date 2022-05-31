784b506f8e570a24c897b89b13a1b5552b9a5787
Alexis Hernandez <alexis22229@gmail.com>
2022-05-31 13:03:52 -0600

    Tweak the data explorer settings
    This is mostly to prevent columns from being updated/displayed.

--- server/src/main/scala/net/wiringbits/modules/DataExplorerModule.scala
+++ server/src/main/scala/net/wiringbits/modules/DataExplorerModule.scala
@@ -1,4 +1,5 @@
 package net.wiringbits.modules
+
 import com.google.inject.{AbstractModule, Provides}
 import net.wiringbits.webapp.utils.admin.config.{DataExplorerSettings, TableSettings}
 
@@ -8,7 +9,12 @@ class DataExplorerModule extends AbstractModule {
   def dataExplorerSettings: DataExplorerSettings = DataExplorerSettings(settings)
 
   val settings = List(
-    TableSettings("users", "user_id", hiddenColumns = List("password"), referenceField = Some("email")),
-    TableSettings("user_logs", "user_log_id")
+    TableSettings(
+      tableName = "users",
+      primaryKeyField = "user_id",
+      hiddenColumns = List("password", "email"),
+      // to prevent garbage data, let's disable most columns
+      nonEditableColumns = List("user_id", "email", "created_at", "verified_on", "name")
+    )
   )
 }
