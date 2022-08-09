67bbb43aec59416c9916c55cdd04b682d73aa798
KappaStromblessed <seb02tian20@gmail.com>
2022-08-09 13:54:47 -0600

    Remove useless code in ResendVerifyEmailPage

--- web/src/main/scala/net/wiringbits/components/pages/ResendVerifyEmailPage.scala
+++ web/src/main/scala/net/wiringbits/components/pages/ResendVerifyEmailPage.scala
@@ -1,34 +1,14 @@
 package net.wiringbits.components.pages
 
-import com.alexitc.materialui.facade.materialUiCore.createMuiThemeMod.Theme
-import com.alexitc.materialui.facade.materialUiStyles.makeStylesMod.StylesHook
-import com.alexitc.materialui.facade.materialUiStyles.mod.makeStyles
-import com.alexitc.materialui.facade.materialUiStyles.withStylesMod.{
-  CSSProperties,
-  StyleRulesCallback,
-  Styles,
-  WithStylesOptions
-}
 import net.wiringbits.AppContext
 import net.wiringbits.components.widgets.ResendVerifyEmailForm
 import net.wiringbits.webapp.utils.slinkyUtils.components.core.widgets.Container
 import net.wiringbits.webapp.utils.slinkyUtils.components.core.widgets.Container.Alignment
-import org.scalablytyped.runtime.StringDictionary
 import slinky.core.FunctionalComponent
 import slinky.core.annotations.react
 
 @react object ResendVerifyEmailPage {
   case class Props(ctx: AppContext)
-
-  private lazy val useStyles: StylesHook[Styles[Theme, Unit, String]] = {
-    val stylesCallback: StyleRulesCallback[Theme, Unit, String] = theme =>
-      StringDictionary(
-        "resendVerifyEmailContainer" -> CSSProperties()
-          .setMaxWidth(350)
-          .setWidth("100%")
-      )
-    makeStyles(stylesCallback, WithStylesOptions())
-  }
 
   val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props =>
     Container(
