d513a9944665f5aa6953b0a6d7be405731605c95
Sebastian Arellanes <80025691+KapStorm@users.noreply.github.com>
2022-09-06 18:30:33 -0600

    Adding information about the app (#282)
    * Adding wiringbits favicon and change html title
    
    * wiringbits logo and repo link in about page
    
    * Adding information about the app in home
    
    * Better snippets images quality
    
    * Adding styles
    
    * Change user and password text to bold
    
    * Center home title
    
    * scalafmt

--- web/src/main/scala/net/wiringbits/components/pages/AboutPage.scala
+++ web/src/main/scala/net/wiringbits/components/pages/AboutPage.scala
@@ -1,21 +1,66 @@
 package net.wiringbits.components.pages
 
+import com.alexitc.materialui.facade.materialUiCore.createMuiThemeMod.Theme
+import com.alexitc.materialui.facade.materialUiCore.{
+  typographyTypographyMod,
+  components => mui,
+  materialUiCoreStrings => muiStrings
+}
+import com.alexitc.materialui.facade.materialUiStyles.makeStylesMod.StylesHook
+import com.alexitc.materialui.facade.materialUiStyles.mod.makeStyles
+import com.alexitc.materialui.facade.materialUiStyles.withStylesMod.{
+  CSSProperties,
+  StyleRulesCallback,
+  Styles,
+  WithStylesOptions
+}
 import net.wiringbits.AppContext
 import net.wiringbits.core.I18nHooks
-import net.wiringbits.webapp.utils.slinkyUtils.components.core.widgets.{Subtitle, Title}
+import net.wiringbits.webapp.utils.slinkyUtils.components.core.widgets.Container
+import net.wiringbits.webapp.utils.slinkyUtils.components.core.widgets.Container.{Alignment, EdgeInsets}
+import org.scalablytyped.runtime.StringDictionary
 import slinky.core.FunctionalComponent
 import slinky.core.annotations.react
 import slinky.core.facade.Fragment
+import slinky.web.html.{alt, className, img, src}
 
 @react object AboutPage {
   case class Props(ctx: AppContext)
 
+  private lazy val useStyles: StylesHook[Styles[Theme, Unit, String]] = {
+    val stylesCallback: StyleRulesCallback[Theme, Unit, String] = theme =>
+      StringDictionary(
+        "image" -> CSSProperties()
+          .setMaxWidth(300)
+          .setMaxHeight(164)
+      )
+    makeStyles(stylesCallback, WithStylesOptions())
+  }
+
   val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props =>
     val texts = I18nHooks.useMessages(props.ctx.$lang)
+    val classes = useStyles(())
 
-    Fragment(
-      Title(texts.aboutPage),
-      Subtitle(texts.projectDetails)
+    val wiringbitsImage =
+      img(src := "/img/wiringbits-logo.png", alt := "wiringbits logo", className := classes("image"))
+    val repositoryLink = mui
+      .Link(texts.checkoutTheRepo)
+      .variant(typographyTypographyMod.Style.h5)
+      .color(muiStrings.inherit)
+      .href("https://github.com/wiringbits/scala-webapp-template")
+      .target("_blank")
+
+    Container(
+      flex = Some(1),
+      alignItems = Alignment.center,
+      margin = EdgeInsets.top(48),
+      child = Fragment(
+        wiringbitsImage,
+        Container(
+          margin = EdgeInsets.top(32),
+          child = repositoryLink
+        )
+      )
     )
   }
 }
