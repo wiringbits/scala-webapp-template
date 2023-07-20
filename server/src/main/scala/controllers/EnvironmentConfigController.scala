7010fec5160146f16a78d1456cac0412d636fcbf
Sebastian <seb02tian20@gmail.com>
2023-07-20 09:54:33 -0600

    EnvironmentConfig routes

--- server/src/main/scala/controllers/EnvironmentConfigController.scala
+++ server/src/main/scala/controllers/EnvironmentConfigController.scala
@@ -1,26 +1,33 @@
 package controllers
 
+import akka.stream.Materializer
 import net.wiringbits.actions.GetEnvironmentConfigAction
 import net.wiringbits.api.models.GetEnvironmentConfig
 import org.slf4j.LoggerFactory
-import play.api.libs.json.Json
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
 import sttp.model.MediaType
+import sttp.tapir.server.play.PlayServerInterpreter
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class EnvironmentConfigController @Inject() (
     getEnvironmentConfigAction: GetEnvironmentConfigAction
-)(implicit cc: ControllerComponents, ec: ExecutionContext)
-    extends AbstractController(cc) {
+)(implicit ec: ExecutionContext, mat: Materializer)
+    extends SimpleRouter {
   private val logger = LoggerFactory.getLogger(this.getClass)
+  private val interpreter = PlayServerInterpreter()
 
-  def getEnvironmentConfig: Action[AnyContent] = handleGET { _ =>
+  private def getEnvironmentConfig: Future[Either[Unit, GetEnvironmentConfig.Response]] = {
     logger.info("Get frontend config")
     for {
       response <- getEnvironmentConfigAction()
-    } yield Ok(Json.toJson(response))
+    } yield Right(response)
+  }
+
+  override def routes: Routes = {
+    interpreter.toRoutes(EnvironmentConfigController.getEnvironmentConfig.serverLogic(_ => getEnvironmentConfig))
   }
 }
 
@@ -29,8 +36,11 @@ object EnvironmentConfigController {
   import sttp.tapir.*
   import sttp.tapir.json.play.*
 
-  private val getEnvironmentConfig = endpoint.get
+  private val baseEndpoint = endpoint
     .in("environment-config")
+    .tag("Misc")
+
+  private val getEnvironmentConfig = baseEndpoint.get
     .out(
       jsonBody[GetEnvironmentConfig.Response]
         .description("Got the config values")
@@ -39,7 +49,7 @@ object EnvironmentConfigController {
     .summary("Get the config values for the current environment")
     .description("These values are required by the frontend app to interact with the backend")
 
-  val routes: List[PublicEndpoint[_, _, _, _]] = List(
+  val routes: List[Endpoint[_, _, _, _, _]] = List(
     getEnvironmentConfig
-  ).map(_.tag("Misc"))
+  )
 }
