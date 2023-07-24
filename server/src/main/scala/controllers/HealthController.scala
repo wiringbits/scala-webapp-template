f4060985b503c493d275187f3d75dd7836b1601b
Sebastian <seb02tian20@gmail.com>
2023-07-24 16:42:43 -0600

    Remove health cookie

--- server/src/main/scala/controllers/HealthController.scala
+++ server/src/main/scala/controllers/HealthController.scala
@@ -11,22 +11,8 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class HealthController @Inject() (implicit ec: ExecutionContext) {
-  private def check: Future[Either[Unit, CookieValueWithMeta]] =
-    Future.successful(
-      Right(
-        CookieValueWithMeta(
-          value = "World",
-          expires = Some(Instant.now().plus(3L, ChronoUnit.DAYS)),
-          maxAge = Some(3L),
-          domain = None,
-          path = None,
-          secure = false,
-          httpOnly = false,
-          sameSite = None,
-          otherDirectives = Map.empty
-        )
-      )
-    )
+  private def check: Future[Either[Unit, Unit]] =
+    Future.successful(Right(()))
 
   def routes: List[ServerEndpoint[AkkaStreams with WebSockets, Future]] = {
     List(HealthController.check.serverLogic(_ => check))
@@ -43,7 +29,6 @@ object HealthController {
 
   private val check = baseEndpoint.get
     .out(emptyOutput.description("The app is healthy"))
-    .out(setCookie("Hello"))
     .summary("Queries the application's health")
 
   val routes: List[Endpoint[_, _, _, _, _]] = List(
