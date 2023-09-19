4028f4042515f4c01168ecd442ee352601bd974f
Sebastian <seb02tian20@gmail.com>
2023-09-18 22:53:04 -0700

    Enum trait

--- lib/common/shared/src/main/scala/net/wiringbits/common/models/enums/BackgroundJobType.scala
+++ lib/common/shared/src/main/scala/net/wiringbits/common/models/enums/BackgroundJobType.scala
@@ -1,9 +1,7 @@
 package net.wiringbits.common.models.enums
 
-import anorm.{Column, ParameterMetaData, ToStatement, TypeDoesNotMatch}
+import enumeratum.EnumEntry
 import enumeratum.EnumEntry.Uppercase
-import enumeratum.{Enum, EnumEntry}
-import play.api.libs.json.{Format, JsString, Reads, Writes}
 
 sealed trait BackgroundJobType extends EnumEntry with Uppercase
 
@@ -14,37 +12,4 @@ object BackgroundJobType extends Enum[BackgroundJobType] {
   case object SendEmail extends BackgroundJobType
 
   val values: IndexedSeq[BackgroundJobType] = findValues
-
-  implicit val backgroundJobTypeColumn: Column[BackgroundJobType] = Column.nonNull[BackgroundJobType] { (value, _) =>
-    value match {
-      case string: String =>
-        withNameInsensitiveOption(string) match
-          case Some(value) => Right(value)
-          case None => Left(TypeDoesNotMatch(s"Unknown background job type: $string"))
-      case _ => Left(TypeDoesNotMatch("Error parsing the background job type"))
-    }
-  }
-
-  implicit val backgroundJobTypeOrdering: Ordering[BackgroundJobType] = Ordering.by(_.entryName)
-
-  implicit val backgroundJobTypeToStatement: ToStatement[BackgroundJobType] =
-    ToStatement[BackgroundJobType]((s, index, v) => s.setObject(index, v.entryName))
-
-  implicit val backgroundJobTypeParameterMetaData: ParameterMetaData[BackgroundJobType] =
-    new ParameterMetaData[BackgroundJobType] {
-      override def sqlType: String = "TEXT"
-
-      override def jdbcType: Int = java.sql.Types.VARCHAR
-    }
-
-  implicit val backgroundJobTypeCustomFormat: Format[BackgroundJobType] = Format[BackgroundJobType](
-    fjs = implicitly[Reads[String]].map(string => withNameInsensitive(string)),
-    tjs = Writes[BackgroundJobType](i => JsString(i.entryName))
-  )
-
-  implicit val backgroundJobTypeCustomReads: Reads[BackgroundJobType] =
-    implicitly[Reads[String]].map(string => withNameInsensitive(string))
-
-  implicit val backgroundJobTypeCustomWrites: Writes[BackgroundJobType] =
-    Writes[BackgroundJobType](i => JsString(i.entryName))
 }
--- lib/common/shared/src/main/scala/net/wiringbits/common/models/enums/UserTokenType.scala
+++ lib/common/shared/src/main/scala/net/wiringbits/common/models/enums/UserTokenType.scala
@@ -1,9 +1,7 @@
 package net.wiringbits.common.models.enums
 
-import anorm.{Column, ParameterMetaData, ToStatement, TypeDoesNotMatch}
-import enumeratum.*
+import enumeratum.EnumEntry
 import enumeratum.EnumEntry.Uppercase
-import play.api.libs.json.{Format, JsString, Reads, Writes}
 
 sealed trait UserTokenType extends EnumEntry with Uppercase
 
@@ -12,36 +10,4 @@ object UserTokenType extends Enum[UserTokenType] {
   case object ResetPassword extends UserTokenType
 
   val values: IndexedSeq[UserTokenType] = findValues
-
-  implicit val userTokenTypeColumn: Column[UserTokenType] = Column.nonNull[UserTokenType] { (value, _) =>
-    value match {
-      case string: String =>
-        withNameInsensitiveOption(string) match
-          case Some(value) => Right(value)
-          case None => Left(TypeDoesNotMatch(s"Unknown user token type: $string"))
-      case _ => Left(TypeDoesNotMatch("Error parsing the user token type"))
-    }
-  }
-
-  implicit val userTokenTypeOrdering: Ordering[UserTokenType] = Ordering.by(_.entryName)
-
-  implicit val userTokenTypeToStatement: ToStatement[UserTokenType] =
-    ToStatement[UserTokenType]((s, index, v) => s.setObject(index, v.entryName))
-
-  implicit val userTokenTypeEmailParameterMetaData: ParameterMetaData[UserTokenType] =
-    new ParameterMetaData[UserTokenType] {
-      override def sqlType: String = "TEXT"
-
-      override def jdbcType: Int = java.sql.Types.VARCHAR
-    }
-
-  implicit val userTokenTypeCustomFormat: Format[UserTokenType] = Format[UserTokenType](
-    fjs = implicitly[Reads[String]].map(string => withNameInsensitive(string)),
-    tjs = Writes[UserTokenType](i => JsString(i.entryName))
-  )
-
-  implicit val userTokenTypeCustomReads: Reads[UserTokenType] =
-    implicitly[Reads[String]].map(string => withNameInsensitive(string))
-
-  implicit val userTokenTypeCustomWrites: Writes[UserTokenType] = Writes[UserTokenType](i => JsString(i.entryName))
 }
